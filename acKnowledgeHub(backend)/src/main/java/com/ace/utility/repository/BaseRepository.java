07a1ebb5c3092b0e5a15a8df54596abe48b95e27
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-03-13 21:46:33 +0630

    add baserepository for customize query

---
+++ acKnowledgeHub(backend)/src/main/java/com/ace/utility/repository/BaseRepository.java
@@ -0,0 +1,13 @@
+package com.ace.utility.repository;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.function.Function;
+
+public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
+
+    <DTO> List<DTO> search(Function<CriteriaBuilder, CriteriaQuery<DTO>> queryFunction);
+}
